c17f0a1ac3d7d91be73e929d8653ab701ecb15be
rodrigoflores2411 <rodrigoflores12345678@gmail.com>
2025-10-15 21:31:54 -0500

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -1,5 +1,5 @@
 <script src="js/api-config.js"></script>
 window.APIPERU = {
   token: "cc727ec197abf458cbc49b71f24f35bab8b7bfcf74423d43bd31506b134a204a",
-  baseUrl: "https://apiperu.dev/api"
+  baseUrl: "https://apiperu.dev/api/dni"
 };
